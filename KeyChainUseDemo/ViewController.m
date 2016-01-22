83dd7ac94da2bc7985236cfacf1db63837fee0e4
jiangtaidi <jiangtd@jiangtddeMacBook-Air.local>
2016-01-22 17:44:04 +0800

    first commit

--- KeyChainUseDemo/ViewController.m
+++ KeyChainUseDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Security/Security.h>
 
 @interface ViewController ()
 
@@ -14,14 +15,138 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self deleteData];
+//    [self updateData];
+//    [self findData];
+//    [self findAttr];
+//    [self storeDataTest];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)deleteData
+{
+    NSString *key = @"pwd";
+    NSString *server = [[NSBundle mainBundle] bundleIdentifier];
+    NSDictionary *queue = @{
+                            (__bridge id)kSecClass:(__bridge id)kSecClassGenericPassword,
+                            (__bridge id)kSecAttrService:server,
+                            (__bridge id)kSecAttrAccount:key
+                            };
+    OSStatus state = SecItemCopyMatching((__bridge CFDictionaryRef)queue, NULL);
+    //存在
+    if (state == errSecSuccess) {
+        OSStatus deleteState = SecItemDelete((__bridge CFDictionaryRef)queue);
+        if (deleteState == errSecSuccess) {
+            NSLog(@"删除成功!!!");
+        }
+    }
+}
+
+-(void)updateData
+{
+    NSString *key = @"pwd";
+    NSString *server = [[NSBundle mainBundle] bundleIdentifier];
+    NSDictionary *queue = @{
+                            (__bridge id)kSecClass:(__bridge id)kSecClassGenericPassword,
+                            (__bridge id)kSecAttrService:server,
+                            (__bridge id)kSecAttrAccount:key
+                            };
+    OSStatus state = SecItemCopyMatching((__bridge CFDictionaryRef)queue, NULL);
+    //存在修改
+    if (state == errSecSuccess) {
+        NSString *newValue = @"new Value";
+        NSData *newData = [newValue dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary *paramDict = @{
+                                    (__bridge id)kSecValueData:newData
+                                    };
+        OSStatus updateState = SecItemUpdate((__bridge CFDictionaryRef)queue, (__bridge CFDictionaryRef)paramDict);
+        if (updateState == errSecSuccess) {
+            NSLog(@"更新成功");
+        }
+    }
+}
+
+//存储数据到钥匙串中
+-(void)storeData
+{
+    NSString *key = @"pwd";
+    NSString *value = @"password";
+    NSData *valueData = [value dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *service = [[NSBundle mainBundle] bundleIdentifier];
+    NSDictionary *dict = @{
+                           (__bridge id)kSecClass:(__bridge id)kSecClassGenericPassword,
+                           (__bridge id)kSecAttrService:service,
+                           (__bridge id)kSecAttrAccount:key,
+                           (__bridge id)kSecValueData:valueData
+                           };
+    CFTypeRef typeResult = NULL;
+    OSStatus state =  SecItemAdd((__bridge CFDictionaryRef)dict, &typeResult);
+    if (state == errSecSuccess) {
+        NSLog(@"store secceed");
+    }
+}
+
+//查找
+-(void)findAttr
+{
+    NSString *key = @"pwd";
+    NSString *service = [[NSBundle mainBundle] bundleIdentifier];
+    NSDictionary *dict = @{
+                           (__bridge id)kSecClass:(__bridge id)kSecClassGenericPassword,
+                           (__bridge id)kSecAttrService:service,
+                           (__bridge id)kSecAttrAccount:key,
+                           (__bridge id)kSecReturnAttributes:(__bridge id)kCFBooleanTrue
+                           };
+    CFDictionaryRef resultDict = NULL;
+    OSStatus state = SecItemCopyMatching((__bridge CFDictionaryRef)dict, (CFTypeRef*)&resultDict);
+    NSDictionary *result = (__bridge_transfer NSDictionary*)resultDict;
+    if (state == errSecSuccess)
+    {
+        NSLog(@"server:%@",result[(__bridge id)kSecAttrService]);
+        NSLog(@"account:%@",result[(__bridge id)kSecAttrAccount]);
+        NSLog(@"assessGroup:%@",result[(__bridge id)kSecAttrAccessGroup]);
+        NSLog(@"createDate:%@",result[(__bridge id)kSecAttrCreationDate]);
+        NSLog(@"modifyDate:%@",result[(__bridge id)kSecAttrModificationDate]);
+    }
+}
+
+//查找数据
+-(void)findData
+{
+    NSString *key = @"pwd";
+    NSString *server = [[NSBundle mainBundle] bundleIdentifier];
+    NSDictionary *queueDict = @{
+                                (__bridge id)kSecClass:(__bridge id)kSecClassGenericPassword,
+                                (__bridge id)kSecAttrService:server,
+                                (__bridge id)kSecAttrAccount:key,
+                                (__bridge id)kSecReturnData:(__bridge id)kCFBooleanTrue
+                                /*(__bridge id)kSecMatchLimit : (__bridge id)kSecMatchLimitAll
+                                 当为kSecMatchLimit时，SecItemCopyMatching第二个参数为CFArrayRef，元素为CFDataRef*/
+                                };
+    CFDataRef dataRef = NULL;
+    OSStatus state = SecItemCopyMatching((__bridge CFDictionaryRef)queueDict, (CFTypeRef*)&dataRef);
+    if (state == errSecSuccess) {
+        NSString *value = [[NSString alloc] initWithData:(__bridge_transfer NSData*)dataRef encoding:NSUTF8StringEncoding];
+        NSLog(@"value:%@",value);
+    }
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
